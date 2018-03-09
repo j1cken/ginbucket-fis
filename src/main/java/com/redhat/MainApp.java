3de3a6deab6e0faffa715827bed8529b266715d5
Torben Jaeger <torben@jit-central.com>
2018-03-09 01:54:24 +0100

    enabled AMQ

--- src/main/java/com/redhat/MainApp.java
+++ src/main/java/com/redhat/MainApp.java
@@ -27,13 +27,9 @@ public class MainApp extends RouteBuilder {
 
         restConfiguration().component("undertow").port(8080).bindingMode(RestBindingMode.auto);
 
-        // rest("/opp").put("/subscribe").to("amqp:queue:subs");
+        rest("/opp").put("/subscribe").to("amqp:queue:subs");
 
-        // from("amqp:queue:subs").setHeader("CamelTelegramChatId", constant(chatId))
-        //         .to("telegram:bots/" + authToken);
-        rest("/opp").put("/subscribe").to("direct:foo");
-
-        from("direct:foo").setHeader("CamelTelegramChatId", constant(chatId)).to("telegram:bots/" + authToken);
+        from("amqp:queue:subs").setHeader("CamelTelegramChatId", constant(chatId)).to("telegram:bots/" + authToken);
 
     }
 
